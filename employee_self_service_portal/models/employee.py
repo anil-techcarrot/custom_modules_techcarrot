f67d427fa130929f7a9cd295b4d8eb143a84c152
techcarrot <147490236+techcarrotOdoo@users.noreply.github.com>
2025-12-29 12:26:33 +0400

    Update employee.py
    Rename employee_id (last line) to ess_employee_id

--- employee_self_service_portal/models/employee.py
+++ employee_self_service_portal/models/employee.py
@@ -78,4 +78,4 @@ class HREmployee(models.Model):
     x_passport_country = fields.Char("Passport Issuing Country")
     x_passport_issue = fields.Date("Passport Issue Date")
     x_passport_expiry = fields.Date("Passport Expiry Date")
-    employee_id = fields.Char("Employee ID")
+    ess_employee_id = fields.Char("Employee ID")
