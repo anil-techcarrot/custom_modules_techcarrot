da694125a7805d00292cbe9fe79b2ceedc1c1f5b
Anil Kondaveni <Anil.kondaveni@techcarrot.ae>
2026-04-28 12:21:04 +0530

    code is commented

--- techcarrotOdoo-Staging/employee_profile_change_request/__manifest__.py
+++ techcarrotOdoo-Staging/employee_profile_change_request/__manifest__.py
@@ -15,7 +15,7 @@
         'security/ir.model.access.csv',
         'data/mail_template_data.xml',
         # 'data/sequence_data.xml',
-        'views/email_templates.xml',
+        # 'views/email_templates.xml',
         'views/hr_profile_change_request_wizard_views.xml',
         'views/hr_profile_change_request_views.xml',
     ],
