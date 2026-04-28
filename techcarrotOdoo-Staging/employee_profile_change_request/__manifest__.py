b7a1241e5bfa0ae7cc0ac830ce90bb469df7b4a8
Anil Kondaveni <Anil.kondaveni@techcarrot.ae>
2026-04-28 14:14:44 +0530

    changed

--- techcarrotOdoo-Staging/employee_profile_change_request/__manifest__.py
+++ techcarrotOdoo-Staging/employee_profile_change_request/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'git add employee_profile_change_request/data/mail_template_data.xml',
+    'name': 'Employee Profile Change Request',
     'version': '1.0',
     'category': 'Human Resources',
     'summary': 'Portal profile changes with HR approval workflow and email notifications',
@@ -14,12 +14,9 @@
     'data': [
         'security/ir.model.access.csv',
         'data/mail_template_data.xml',
-        # 'data/sequence_data.xml',
-        # 'views/email_templates.xml',
         'views/hr_profile_change_request_wizard_views.xml',
         'views/hr_profile_change_request_views.xml',
     ],
-
     'installable': True,
     'auto_install': False,
     'application': False,
